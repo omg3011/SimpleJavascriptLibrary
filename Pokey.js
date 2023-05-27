0134f0b85544042b639f3797a030dfb15e128484
LT-JOELONG\JOELONG <joel.ong@activate.sg>
2023-05-27 21:49:53 +0800

    Final Push

--- Pokey.js
+++ Pokey.js
@@ -1,141 +1,95 @@
-;(function(global, $) {
-    
-    // 'new' an object
-    var Greetr = function(firstName, lastName, language) {
-        return new Greetr.init(firstName, lastName, language);   
-    }
-    
-    // hidden within the scope of the IIFE and never directly accessible
-    var supportedLangs = ['en', 'es'];
-    
-    // informal greetings
-    var greetings = {
-        en: 'Hello',
-        es: 'Hola'
-    };
-    
-    // formal greetings
-    var formalGreetings = {
-        en: 'Greetings',
-        es: 'Saludos'
-    };
-    
-    // logger messages
-    var logMessages = {
-        en: 'Logged in',
-        es: 'Inició sesión'
-    };
-    
-    // prototype holds methods (to save memory space)
-    Greetr.prototype = {
-        
-        // 'this' refers to the calling object at execution time
-        fullName: function() {
-            return this.firstName + ' ' + this.lastName;   
-        },
-        
-        validate: function() {
-            // check that is a valid language
-            // references the externally inaccessible 'supportedLangs' within the closure
-             if (supportedLangs.indexOf(this.language)  === -1) {
-                throw "Invalid language";   
-             }
-        },
-        
-        // retrieve messages from object by referring to properties using [] syntax
-        greeting: function() {
-            return greetings[this.language] + ' ' + this.firstName + '!';
-        },
-        
-        formalGreeting: function() {
-            return formalGreetings[this.language] + ', ' + this.fullName();  
-        },
-        
-        // chainable methods return their own containing object
-        greet: function(formal) {
-            var msg;
-            
-            // if undefined or null it will be coerced to 'false'
-            if (formal) {
-                msg = this.formalGreeting();  
-            }
-            else {
-                msg = this.greeting();  
-            }
+// Create a IFFE
+;(function (global, $) {
 
-            if (console) {
-                console.log(msg);
-            }
+  // 'new' an object
+  var Pokey = function(firstName, lastName, language) {
+    return new Pokey.init(firstName, lastName, language);
+  }
+  Pokey.init = function(firstName, lastName, language) {
+    var self = this;
 
-            // 'this' refers to the calling object at execution time
-            // makes the method chainable
-            return this;
-        },
-        
-        log: function() {
-            if (console) {
-                console.log(logMessages[this.language] + ': ' + this.fullName()); 
-            }
-            
-            // make chainable
-            return this;
-        },
-                            
-        setLang: function(lang) {
-            
-            // set the language
-            this.language = lang;
-        
-            // validate
-            this.validate();
-            
-            // make chainable
-            return this;
-        },
-        
-        HTMLGreeting: function(selector, formal) {
-            if (!$) {
-                throw 'jQuery not loaded';   
-            }
-            
-            if (!selector) {
-                throw 'Missing jQuery selector';   
-            }
-            
-            // determine the message
-            var msg;
-            if (formal) {
-                msg = this.formalGreeting();   
-            }
-            else {
-                msg = this.greeting();   
-            }
-            
-            // inject the message in the chosen place in the DOM
-            $(selector).html(msg);
-            
-            // make chainable
-            return this;
-        }
-        
-    };
+    self.firstName = firstName;
+    self.lastName = lastName;
+    self.language = language;
+
+    self.validateLanguage();
+  }
+
+  // Variables accessible only in this IFFE scope
+  var supportedLanguages = ['en', 'es'];
+  var informalGreetings = {
+    en: 'hello',
+    es: 'hola',
+  };
+  var formalGreetings = {
+    en: 'Greeting',
+    es: 'Saludos',
+  };
+
+  // Handling prototype
+  //-- This prototype holds public methods (to save memory space)
+  Pokey.prototype = {
+    //------------- Non-chainable functions
+    userFullName: function() {
+      return this.firstName + ' ' + this.lastName;
+    },
+    validateLanguage: function() {
+      if(supportedLanguages.indexOf(this.language) === -1) 
+        throw 'Invalid Language!'; 
+    },
+    userInformalGreeting: function() {
+      return informalGreetings[this.language] + ' ' + this.userFullName();
+    },
+    userFormalGreeting: function() {
+      return formalGreetings[this.language] + ' ' + this.userFullName();
+    },
     
-    // the actual object is created here, allowing us to 'new' an object without calling 'new'
-    Greetr.init = function(firstName, lastName, language) {
-        
-        var self = this;
-        self.firstName = firstName || '';
-        self.lastName = lastName || '';
-        self.language = language || 'en';
-        
-        self.validate();
-        
+    //------------- Chainable functions
+    userGreet: function (isFormal) {
+      var msg;
+
+      // DEtermine what msg to print
+      if(isFormal) {
+        msg = this.userFormalGreeting();
+      } else {
+        msg = this.userInformalGreeting();
+      }
+
+      // Printout msg
+      if(console) {
+        console.log(msg);
+      }
+
+      // To allow chaining
+      return this;
+    },
+    setLanguage: function (lang) {
+      this.language = lang;
+
+      this.validateLanguage();
+
+      // Make chainable
+      return this;
+    },
+    HTMLUserGreeting: function(selector, isFormal) {
+      if(!$) throw 'jQuery not loaded';
+      if(!selector) throw 'Missing jQuery selector';
+
+      // Determine message
+      if(isFormal) {
+        msg = this.userFormalGreeting();
+      } else {
+        msg = this.userInformalGreeting();
+      }
+
+      // Inject message into html using jQuery
+      $(selector).html(msg);
+
+      return this;
     }
-    
-    // trick borrowed from jQuery so we don't have to use the 'new' keyword
-    Greetr.init.prototype = Greetr.prototype;
-    
-    // attach our Greetr to the global object, and provide a shorthand '$G' for ease our poor fingers
-    global.Greetr = global.G$ = Greetr;
-    
+  };
+  Pokey.init.prototype = Pokey.prototype;
+
+  // Attach our pokey to global object and make a short alias for it
+  global.Pokey = global.P$ = Pokey;
 }(window, jQuery));
--- app.js
+++ app.js
@@ -1,3 +1,35 @@
-var g = G$('John', 'Doe');
+// Demo: Console log 
+var p1 = P$('Joel', 'Ong', 'en');
+p1.userGreet(true).setLanguage('es').userGreet(true);
 
-g.greet().setLang('es').greet(true).log();
+// Demo: Html button clicking
+$('#login').click(function () {
+  // Create a new Pokey object
+  var p2 = P$('Sexy', 'Boi', 'en');
+
+  // Hide login on the screen
+  $('#logindiv').hide();
+
+  // Pass Greet msg to html
+  p2.setLanguage($('#lang').val()).HTMLUserGreeting('#greeting', true);
+});
+
+/** Pokemon features
+ * 
+ * // Non-chainable
+ * 1. userFullName()
+ * 2. validateLanguage()
+ * 3. userInformalGreeting()
+ * 4. userFormalGreeting()
+ * 
+ * // Chainable
+ * 1. userGreet(bool)
+ * 2. setLanguage(string)
+ * 3. HTMLUserGreeting(string, bool)
+ * 
+ */
+
+
+
+
+
